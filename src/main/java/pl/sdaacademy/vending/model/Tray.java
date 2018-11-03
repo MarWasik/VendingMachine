babe22f2b0bcf1a79c5141cedc6ec9bd8caa2ec4
Marcin Wasik <wasikmarcin89@gmail.com>
2018-11-03 15:54:58 +0100

    commit after 7h

---
+++ src/main/java/pl/sdaacademy/vending/model/Tray.java
@@ -0,0 +1,14 @@
+package pl.sdaacademy.vending.model;
+
+public class Tray {
+
+    private String symbol;
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public Tray(String symbol) {
+        this.symbol = symbol;
+    }
+}
